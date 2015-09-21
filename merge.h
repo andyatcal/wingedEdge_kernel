71ff3893e21194a38757057ff6455225f1552695
Andy Wang <andyatcal@berkeley.edu>
2015-09-20 17:39:23 -0700

    Commit merge

---
+++ merge.h
@@ -0,0 +1,21 @@
+// 
+// @author Andy Wang, UC Berkeley. Copyright 2015 reserve.
+// Catmull-Clark subdivision project in order to create sculptures like Eva-Hilds.
+// Advised by Prof. Sequin H. Carlos.
+// 
+//
+
+#ifndef __MERGE_H__
+#define __MERGE_H__
+
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+using namespace glm;
+
+//////////////////////////////////////////////////////////////////////
+// Mesh Class -- A MESH is a collection of polygon facets
+class Merge
+
+#endif // __MESH_H__
