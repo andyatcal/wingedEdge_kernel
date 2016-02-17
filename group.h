f28a6bc6a274239bfa88e37a457bfefe3acd125d
andyatcal <andyatcal@berkeley.edu>
2016-02-16 16:34:20 -0800

    group update

--- group.h
+++ group.h
@@ -18,24 +18,27 @@ using namespace glm;
 class Mesh;
 
 //////////////////////////////////////////////////////////////////////
-// Mesh Class -- A MESH is a collection of polygon facets
+// Group Class -- A Group is a collection of meshes and its subgroups
 class Group{
 public:
     // Constructor.
     Group();
     // Add one mesh to the current group
-    // @param vertices is a list of consequtive vertices of the face.
+    // @param mesh, the mesh to be added.
+    // @param transformation, the transformation of this mesh.
     void addMesh(Mesh* mesh, mat4 transformation);
+    // Remove one mesh to the current group
     // @param vertices is a list of consequtive vertices of the face.
     void removeMesh(Mesh* mesh);
-    // Now think about what instances of this group would have?
-    // It has a collection of mesh pointers and the transformations
+    // add subgroup for this Group
+    void addSubGroup(Group* group, mat4 transformation);
+    // A group is a collection of mesh pointers and the transformations
     // for each one of them.
     unordered_map<Mesh*, mat4> meshes;
     // Also, it contains pointers to the subgroups so we can build
     // hirachinal scene.
-    vector<Group*> subgroups;
-    // The hashMap of this group
+    unordered_map<Group*, mat4> subgroups;
+    // It can also have a special color of this group;
 };
 
 Group::Group() {}
@@ -47,4 +50,6 @@ void Group::addMesh(Mesh* mesh, mat4 transformation) {
 void Group::removeMesh(Mesh* mesh) {
     meshes.erase(mesh);
 }
+
+    void addGroup(Group* group);
 #endif // __GROUP_H__
---
+++ scene.cpp
@@ -0,0 +1,31 @@
+// 
+// @author Andy Wang, UC Berkeley. Copyright 2015 reserve.
+// Catmull-Clark subdivision project in order to create sculptures like Eva-Hilds.
+// Advised by Prof. Sequin H. Carlos.
+// 
+//
+
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#include <vector>
+
+using namespace std;
+using namespace glm;
+
+// Forward declarations
+class Group;
+
+//////////////////////////////////////////////////////////////////////
+// Scene Class -- A scene is a collection of groups and lighting conditions
+class Scene{
+public:
+    // Constructor.
+    Scene();
+    // Add Group function.
+    // The instances in this scene
+};
+
+Scene::Scene() {}
+
+#endif // __SCENE_H__
